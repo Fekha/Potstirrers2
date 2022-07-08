c7bf3efd23203051c1572903025c9341f3d9b92c
James Moran <james.moranjr@yahoo.com>
2022-07-08 08:56:52 -0400

    ui

---
+++ Assets/Scripts/TabController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabController : MonoBehaviour
+{
+    public GameObject StorePanel;
+    public Image StoreButtonImage;
+    public GameObject FriendPanel;
+    public Image FriendButtonImage;
+    public GameObject HomePanel;
+    public Image HomeButtonImage;
+    public GameObject ChestsPanel;
+    public Image ChestsButtonImage;
+    public GameObject CollectionPanel;
+    public Image CollectionButtonImage;
+    public Sprite SelectedTabSprite;
+    public Sprite UnselectedTabSprite;
+
+    public void TabClicked(int Selected)
+    {
+        if (Selected != 1)
+        {
+            StorePanel.SetActive(false);
+            StoreButtonImage.sprite = UnselectedTabSprite;
+        }
+        else
+        {
+            StorePanel.SetActive(true);
+            StoreButtonImage.sprite = SelectedTabSprite;
+        }
+        if (Selected != 2)
+        {
+            FriendPanel.SetActive(false);
+            FriendButtonImage.sprite = UnselectedTabSprite;
+        }
+        else
+        {
+            FriendPanel.SetActive(true);
+            FriendButtonImage.sprite = SelectedTabSprite;
+        }
+        if (Selected != 3)
+        {
+            HomePanel.SetActive(false);
+            HomeButtonImage.sprite = UnselectedTabSprite;
+        }
+        else
+        {
+            HomePanel.SetActive(true);
+            HomeButtonImage.sprite = SelectedTabSprite;
+        }
+        if (Selected != 4)
+        {
+            ChestsPanel.SetActive(false);
+            ChestsButtonImage.sprite = UnselectedTabSprite;
+        }
+        else
+        {
+            ChestsPanel.SetActive(true);
+            ChestsButtonImage.sprite = SelectedTabSprite;
+        }
+        if (Selected != 5)
+        {
+            CollectionPanel.SetActive(false);
+            CollectionButtonImage.sprite = UnselectedTabSprite;
+        }
+        else
+        {
+            CollectionPanel.SetActive(true);
+            CollectionButtonImage.sprite = SelectedTabSprite;
+        }
+    }
+}
